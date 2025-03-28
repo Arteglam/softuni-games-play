6ca5092121b2f7c62aba59aa6bd86386e5db2126
ArmorDude <legend.ivanov@gmail.com>
2025-03-28 20:41:33 +0200

    Add use games api

--- client/src/api/gameApi.js
+++ client/src/api/gameApi.js
@@ -1,15 +1,11 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
 import  request  from "../utils/request";
 
 const baseUrl = 'http://localhost:3030/data/games';
 
 export default {
-    async getAll() {
-        const result = await request.get(baseUrl);
-        const games = Object.values(result);
-        return games;
-    },
+
     getOne(gameId) {
         return request.get(`${baseUrl}/${gameId}`);
     },
@@ -23,6 +19,18 @@ export default {
 
 };
 
+export const useGames = () => {
+    const [games, setGames] = useState([]);
+
+    useEffect(()=> {
+        request.get(baseUrl)
+            .then(setGames)
+    }, []);
+    return {
+        games,
+    }
+}
+
 export const useCreateGame = () => {
     const { accessToken } = useContext(UserContext);
 
@@ -31,7 +39,7 @@ export const useCreateGame = () => {
             'X-Authorization': accessToken
         }
     };
-    
+
     const create = (gameData) => 
        request.post(baseUrl, gameData, options);
    
